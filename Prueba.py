b2b895914f37e98b489afae606111d323aab6379
Marco Octavio luna Vargas <marco.octavio.luna@gmail.com>
2021-03-10 18:01:14 -0400

    Commit 0, add Prueba.py, se programo una prueba para probar python

---
+++ Prueba.py
@@ -0,0 +1,8 @@
+#VARIABLES
+num = int(input("Escribe un numero: "))
+num2 = int(input("Escribe otro numero: "))
+
+#PROGRESO
+sum = num + num2
+resultado = str(num) + " + " + str(num2) + " = " + str(sum)
+print(resultado)
