1ee1db698532a3d0f871f1c1004bd500eac061e1
Marco Octavio luna Vargas <marco.octavio.luna@gmail.com>
2021-03-12 10:25:53 -0400

    Commit 3, add Funciones.py, se programo las funciones

---
+++ funciones.py
@@ -0,0 +1,50 @@
+#VARIABLES
+
+#FUNCIONES
+def num(numero):
+    print("""HOLA
+    ESCOGISTE LA OPCION """ + numero)
+    
+
+#PROGRESO
+opcion = int(input("Elige 3 opciones (1, 2, 3): "))
+if opcion == 1:
+    num('1')
+elif opcion == 2:
+    num("2")
+elif opcion == 3:
+    num("3")
+else:
+    print("INTENTA DE NUEVO...")
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #def imprimirMensajes(): 
+#    print("Mensaje especial")
+#    print("Estoy aprendiendo a usar funciones")
+
+
+#imprimirMensajes()
+#imprimirMensajes()
+#imprimirMensajes()
