026bd5da992146164373594c8a3cece003c8db99
Marco Octavio luna Vargas <marco.octavio.luna@gmail.com>
2021-03-25 15:55:23 -0400

    Commit 26, add Funciones-Algebraicas-Funcion-Polinomial, Funciones-Algebraicas-Funcion-Lineal, Funciones-Algebraicas-Funcion-Polinomial-2, Funciones-Transcendentes-Funcion-Logaritmica, Funciones-Transcendentes-Funcion-Exponencial, Funciones-Transcendentes-Funcion-Trigonometricas; se programos todo este estipo de funcion en python

---
+++ CALCULO-BASICO-PYTHON/Funciones-Algebraicas-Funcion-Lineal.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Esta es la funcion algebraica
+#Sus valores siempre tiene que ser m = Dominio, x = variable independiente, b = es el punto de corte con la eje y
+def f(m, x, b):
+  #Estamos regresando la funcion lineal
+  return m*x + b
+
+#esto es cuantos puntos tendra nuestro vector 
+n = 100
+#Este es el dominio o la pendiente, es una constante pertenece a los numeros reales
+m = 4
+#Este es el corte de el corte del punto de la eje y se donde pase la linea en el eje y
+b = -8
+#esta es la variable independiente 
+#np.lispace es un vector que nos genera una serie de puntos en un rango
+#(-1, 5,) esto es para ver cual sera el punnto de partida y de llegada, -1 es el punto de partida y 5 es donde termina el vector
+#(num=res) estos son cuantos puntos tiene nuestro vector, num es el parametro que lo define
+x = np.linspace(-5,  5, num=n)
+#Estamos pasando los valores a nuestr funcion
+y = f(m,x,b)
+#Estamos graficando nuestra funcion lineal, matplotlib estamos usando esta libreria
+#fig es para optener la figura
+#ax es para optener los eje de la grafica que estamos haciendo
+#plt es la funcion matplotlib, subplots() es para que se pueda graficar
+fig, ax = plt.subplots()
+#En esta parte porfin estamos graficando
+#ax.plot es para graficar
+#(x, y) la grafica nos pide el dominio de la funcion que es el "x", y nos pide la funcion que es la "y"
+ax.plot(x, y)
+#Esto es para cuadricular el plano carteciano
+ax.grid()
+#Esto es para poner lineas al plano, en este claso estamos poniendo una linea en horizontal
+#ax es para graficar
+#axhline es para graficar la linea horizontal que es la linea de la "X" en un plano carteciano
+#y = 0 es para que la linea se pongo en el eje de y = 0 por que en ese lugar se encuanta el eje de las x
+#color es para ponerle un color a la linea, r es rojo
+ax.axhline(y = 0, color="r")
+#Esto es para poner lineas al plano, en este claso estamos poniendo una linea en vertical
+#ax es para graficar
+#axvline es para graficar la linea vertical que es la linea de la "Y" en un plano carteciano
+#x = 0 es para que la linea se pongo en el eje de x = 0 por que en ese lugar se encuanta el eje de las y
+#color es para ponerle un color a la linea, r es rojo
+ax.axvline(x = 0, color="r")
---
+++ CALCULO-BASICO-PYTHON/Funciones-Transcendentes-Funcion-Logaritmica.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Esta es la funcion algebraica logaritmica
+def f(x):
+  #Estamos regresando la funcion logaritmica
+  return np.log2(x)         
+
+#esto es cuantos puntos tendra nuestro vector -Funcion-Locaritmica
+n = 1000
+#esta es la variable independiente 
+#np.lispace es un vector que nos genera una serie de puntos en un rango
+#(0.01, 5,) esto es para ver cual sera el punnto de partida y de llegada, 0.01 es el punto de partida y 5 es donde termina el vector
+#(num=res) estos son cuantos puntos tiene nuestro vector, num es el parametro que lo define
+#Estamos usando como partida 0.01 por que la funcion noslo pide
+x = np.linspace(0.01,  256, num=n)
+#Estamos pasando los valores a nuestro funcion
+y = f(x)
+#Estamos graficando nuestra funcion lineal, matplotlib estamos usando esta libreria
+#fig es para optener la figura
+#ax es para optener los eje de la grafica que estamos haciendo
+#plt es la funcion matplotlib, subplots() es para que se pueda graficar
+fig, ax = plt.subplots()
+#En esta parte porfin estamos graficando
+#ax.plot es para graficar
+#(x, y) la grafica nos pide el dominio de la funcion que es el "x", y nos pide la funcion que es la "y"
+ax.plot(x, y)
+#Esto es para cuadricular el plano carteciano
+ax.grid()
+#Esto es para poner lineas al plano, en este claso estamos poniendo una linea en horizontal
+#ax es para graficar
+#axhline es para graficar la linea horizontal que es la linea de la "X" en un plano carteciano
+#y = 0 es para que la linea se pongo en el eje de y = 0 por que en ese lugar se encuanta el eje de las x
+#color es para ponerle un color a la linea, r es rojo
+ax.axhline(y = 0, color="r")
+#Esto es para poner lineas al plano, en este claso estamos poniendo una linea en vertical
+#ax es para graficar
+#axvline es para graficar la linea vertical que es la linea de la "Y" en un plano carteciano
+#x = 0 es para que la linea se pongo en el eje de x = 0 por que en ese lugar se encuanta el eje de las y
+#color es para ponerle un color a la linea, r es rojo
+ax.axvline(x = 0, color="r")
+ax.axvline(x=0, color='r')
